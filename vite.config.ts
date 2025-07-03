4bbd2245477011c5121f10a079b043d02b448339
Ebad84 <ebad84.025@gmail.com>
2025-07-03 20:21:04 +0330

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/REPOSITORY-NAME/', // اسم repository رو اینجا بذار
+  base: '/personal-website-v1/', // اسم repository رو اینجا بذار
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
